13025d958d46196fedc633c956cf573f0fd4b94e
MontesCodes-MacBookProStation <montieswebdev@gmail.com>
2022-03-13 21:22:05 -0400

    js update 3/13/22 #1

--- js/script.js
+++ js/script.js
@@ -13,8 +13,16 @@ class FlowFieldEffect {
   #height;
   constructor(ctx, width, height) {
     this.#ctx = ctx;
+    this.#ctx.strokeStyle = 'white';
     this.#width = width;
     this.#height = height;
     console.log('effect loaded');
+    this.#draw(100, 100);
+  }
+  #draw(x, y) {
+    this.#ctx.beginPath();
+    this.#ctx.moveTo(x, y);
+    this.#ctxlineTo(x + 300, y + 300);
+    this.#ctx.stroke();
   }
 }
