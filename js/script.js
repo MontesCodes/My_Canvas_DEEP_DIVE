13b767afa70afae84a7629842760932259430cf9
Monties Kelly <mokelly@cchs.com>
2022-03-15 18:09:36 -0400

    JS update 3/15/22 #1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ window.onload = function () {
   const ctx = canvas.getContext('2d');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  const folwField = new FlowFieldEffect();
+  const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+  flowField.animate();
 };
 
 class FlowFieldEffect {
@@ -20,9 +21,16 @@ class FlowFieldEffect {
     this.#draw(100, 100);
   }
   #draw(x, y) {
+    const lenth = 300;
     this.#ctx.beginPath();
     this.#ctx.moveTo(x, y);
-    this.#ctxlineTo(x + 300, y + 300);
+    // this.#ctxlineTo(x + 300, y + 300);
+    this.#ctx.lineTo(x + length, y + length);
     this.#ctx.stroke();
+  }
+  animate() {
+    this.#draw(100, 100);
+    console.log('animating');
+    requestAnimationFrame(this.animate.bind(this));
   }
 }
